e9fef1bbac49700b2a91961c083c46f58e47c291
王正东 <wangzhengdong@ecmoban.com>
2021-05-17 18:13:56 +0800

    add module royalcms-component-contracts.

---
+++ royalcms-component-contracts/src/main/java/io/github/mavenreposs/component/contracts/storage/Cloud.java
@@ -0,0 +1,13 @@
+package io.github.mavenreposs.component.contracts.storage;
+
+public interface Cloud extends Storage {
+
+    /**
+     * Get the URL for the file at the given path.
+     *
+     * @param  path url path
+     * @return string
+     */
+    String url(String path);
+
+}
