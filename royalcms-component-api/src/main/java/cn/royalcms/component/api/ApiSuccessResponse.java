6883a4a9a04934480cd61f9a13f4bfef03a0edf6
Wang Zhengdong <hxroyal@gmail.com>
2021-09-06 18:02:50 +0800

    优化POM依赖

--- royalcms-component-api/src/main/java/cn/royalcms/component/api/ApiSuccessResponse.java
+++ royalcms-component-api/src/main/java/cn/royalcms/component/api/ApiSuccessResponse.java
@@ -1,6 +1,7 @@
 package cn.royalcms.component.api;
 
-import cn.royalcms.component.facades.RC_Time;
+
+import cn.royalcms.component.facades.time.RC_Time;
 
 public class ApiSuccessResponse implements ApiResponseInterface {
 
