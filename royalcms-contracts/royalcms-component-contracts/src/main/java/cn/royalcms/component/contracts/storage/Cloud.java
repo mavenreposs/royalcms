6ee9442af5803506a39d4505f44d569d222744ce
Wang Zhengdong <hxroyal@gmail.com>
2021-09-13 09:32:23 +0800

    移动目录

---
+++ royalcms-contracts/royalcms-component-contracts/src/main/java/cn/royalcms/component/contracts/storage/Cloud.java
@@ -0,0 +1,13 @@
+package cn.royalcms.component.contracts.storage;
+
+public interface Cloud extends Storage {
+
+    /**
+     * Get the URL for the file at the given path.
+     *
+     * @param  path url path
+     * @return string
+     */
+    String url(String path);
+
+}
