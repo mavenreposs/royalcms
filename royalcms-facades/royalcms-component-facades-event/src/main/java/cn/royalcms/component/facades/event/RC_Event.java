820b0ccf5a44752ce5cd39b74ad820daac5931a5
Wang Zhengdong <hxroyal@gmail.com>
2021-09-13 09:33:06 +0800

    移动目录

---
+++ royalcms-facades/royalcms-component-facades-event/src/main/java/cn/royalcms/component/facades/event/RC_Event.java
@@ -0,0 +1,19 @@
+package cn.royalcms.component.facades.event;
+
+import cn.royalcms.component.bean.SpringBeanFactory;
+import org.springframework.context.ApplicationEvent;
+
+public class RC_Event {
+
+    private RC_Event() {
+    }
+
+    public static void fire(ApplicationEvent event) {
+        event(event);
+    }
+
+    public static void event(ApplicationEvent event) {
+        SpringBeanFactory.getApplicationContext().publishEvent(event);
+    }
+
+}
