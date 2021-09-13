1bdd2705edb75454169984be8da87ddfad0420de
Wang Zhengdong <hxroyal@gmail.com>
2021-09-13 09:32:15 +0800

    移动目录

--- royalcms-component-upload/src/main/java/cn/royalcms/component/upload/SubDirnameRule.java
+++
@@ -1,6 +0,0 @@
-package cn.royalcms.component.upload;
-
-import java.util.function.Function;
-
-public interface SubDirnameRule extends Function<String, String> {
-}
--- royalcms-component-upload/src/main/java/cn/royalcms/component/upload/UploadException.java
+++
@@ -1,9 +0,0 @@
-package cn.royalcms.component.upload;
-
-import java.io.IOException;
-
-public class UploadException extends IOException {
-    public UploadException(String s) {
-        super(s);
-    }
-}
