df9ba81be1ae51f1e9e93d62e8bbff6a27f204b2
Wang Zhengdong <hxroyal@gmail.com>
2021-09-13 09:34:10 +0800

    移动目录

---
+++ royalcms-facades/royalcms-component-facades-upload/src/main/java/cn/royalcms/component/facades/upload/RC_Upload.java
@@ -0,0 +1,34 @@
+package cn.royalcms.component.facades.upload;
+
+
+import cn.royalcms.component.facades.storage.RC_Storage;
+import cn.royalcms.component.upload.*;
+
+public class RC_Upload {
+
+    private RC_Upload() {
+    }
+
+    public static Uploader uploader(UploadSelecter uploadSelecter) {
+        return uploader(uploadSelecter.getClassInstance());
+    }
+
+    public static <T extends Uploader> Uploader uploader(T uploader) {
+        return uploader(uploader, null);
+    }
+
+    public static Uploader uploader(UploadSelecter uploadSelecter, UploadOption option) {
+        return uploader(uploadSelecter.getClassInstance(), option);
+    }
+
+    public static <T extends Uploader> Uploader uploader(T uploader, UploadOption option) {
+        if (option != null) {
+            uploader.setUploadOption(option);
+        }
+        uploader.setDisk(RC_Storage.disk().orElse(null));
+        uploader.setFilenameCallback(new RandomFilenameRule());
+        uploader.setSubDirnameCallback(new UploadSubDirRule());
+        return uploader;
+    }
+
+}
