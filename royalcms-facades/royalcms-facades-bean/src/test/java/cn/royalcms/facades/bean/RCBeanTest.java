12ddaeba055fe24303c248545fae9d0a4f0321b4
Wang Zhengdong <hxroyal@gmail.com>
2021-12-28 16:42:34 +0800

    更新单元测试

--- royalcms-facades/royalcms-facades-bean/src/test/java/cn/royalcms/facades/bean/RCBeanTest.java
+++ royalcms-facades/royalcms-facades-bean/src/test/java/cn/royalcms/facades/bean/RCBeanTest.java
@@ -1,7 +1,10 @@
 package cn.royalcms.facades.bean;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * ClassName RCBeanTest
@@ -11,9 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author Zhengdong Wang
  * @version jdk11
  */
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApplication.class)
 public class RCBeanTest {
 
+    @DisplayName("Test getBean successful")
     @Test
     public void testBean() {
         System.out.println(RC_Bean.getBean(SpringBeanFactory.class));
